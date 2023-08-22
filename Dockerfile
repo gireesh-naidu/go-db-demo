5831e92288846892edb2117ade9a65eb60aaa065
Gireesh Naidu <gireesh@devtron.ai>
2023-08-22 18:50:03 +0530

    test commit

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,10 @@
 
 FROM golang:1.19.0-alpine3.16
-
 RUN apk add --no-cache git
-
-# Set the Current Working Directory inside the container
 WORKDIR /go/src/github.com/ghgsnaidu/go-db-demo
-
-# We want to populate the module cache based on the go.{mod,sum} files.
-#COPY go.mod .
-#COPY go.sum .
-#
-#RUN echo $GOPATH
-#RUN go mod tidy
-
 COPY . .
 RUN go mod tidy
 WORKDIR /go/src/github.com/ghgsnaidu/go-db-demo/cmd/main
-ENV DBURL="root:root@tcp(172.17.0.2:3306)/mydb"
-# Build the Go app
 RUN go build   -o app
-#-o ./go-db-demo/cmd/main
-
-
-# This container exposes port 3000 to the outside world
-
 EXPOSE 3000
-# Run the binary program produced by `go install`
 CMD ["./app"]
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,41 +2,35 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/ghgsnaidu/go-db-demo/pkg/dbconfig"
-	"github.com/ghgsnaidu/go-db-demo/pkg/routers"
-	"github.com/ghgsnaidu/go-db-demo/pkg/services"
-	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	fmt.Println("hello gireesh!!")
 	//created router
-	router := mux.NewRouter()
-	//wire.Build(dbconfig.NewConfig, dbconfig.InitDB, services.InitServices)
-	//getting db configurations
-
-	fmt.Println("........router created")
-	fmt.Println("added test log message")
-	config := dbconfig.NewConfig()
-
-	//opening db connc by injecting config and getting db instance
-	fmt.Println("connecting to db")
-	db := dbconfig.InitDB(config)
-
-	//injecting db into services layer
-	services.InitServices(db)
-
-	routers.RegisterRoutes(router)
-	http.Handle("/", router)
-
-	fmt.Println("server starting......")
-	err := http.ListenAndServe(":3000", router)
-	if err != nil {
-		fmt.Println("error starting server...")
-		return
-	}
+	//router := mux.NewRouter()
+	////wire.Build(dbconfig.NewConfig, dbconfig.InitDB, services.InitServices)
+	////getting db configurations
+	//
+	//fmt.Println("........router created")
+	//fmt.Println("added test log message")
+	//config := dbconfig.NewConfig()
+	//
+	////opening db connc by injecting config and getting db instance
+	//fmt.Println("connecting to db")
+	//db := dbconfig.InitDB(config)
+	//
+	////injecting db into services layer
+	//services.InitServices(db)
+	//
+	//routers.RegisterRoutes(router)
+	//http.Handle("/", router)
+	//
+	//fmt.Println("server starting......")
+	//err := http.ListenAndServe(":3000", router)
+	//if err != nil {
+	//	fmt.Println("error starting server...")
+	//	return
+	//}
 
 }
